ad9f05da507f4dc4cb4b0a4bc181b3ad204b71c8
Johannes ter Haak <jojo@terhaak.de>
2016-03-27 14:45:58 +0200

    added shell script for cron execution

---
+++ check_updates.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+MAIL_TO=jojo@terhaak.de
+CHECK_APT_APP=/opt/check_apt_updates/app.py
+
+/usr/bin/apt-get -qq --assume-no update
+CHECK_APT_APP --headers --to $MAIL_TO | /usr/sbin/sendmail -t -i
