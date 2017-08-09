b294b9885b4c65f9d61497c70eb35605dcf59915
Johannes ter Haak <jojo@terhaak.de>
2017-08-09 19:53:05 +0200

    updated shell script

--- check_updates.sh
+++ check_updates.sh
@@ -3,4 +3,4 @@ MAIL_TO=jojo@terhaak.de
 CHECK_APT_APP=/opt/check_apt_updates/app.py
 
 #/usr/bin/apt-get -qq --assume-no update
-$CHECK_APT_APP --headers --to $MAIL_TO | /usr/sbin/sendmail -t -i
+$CHECK_APT_APP mail --headers --to $MAIL_TO | /usr/sbin/sendmail -t -i
