c09851a320e3ccbf413b6b2e41e0f3e56fb82dab
Johannes ter Haak <jojo@terhaak.de>
2017-08-09 19:51:11 +0200

    added nagios and list output

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import shutil
 import socket
 import argparse
+import sys
 
 from errors import FatalError
 import TableWriter
@@ -13,19 +14,52 @@ class app(object):
         self.upgrades = None
         self.email_to = "root"
         self.email_from = "root"
+        self.subcommand = "list"
+        self.warn_thres = 10
+        self.critical_thres = 30
+        self.returncode = 0
+        self.use_colors = True;
+        self.colors = {
+            "default": "\033[39m", "red": "\033[31m", "green": "\033[32m", 
+            "yellow" : "\033[33m", "blue": "\033[34m", "magenta": "\033[35m",
+            "light_red": "\033[91m", "light_green": "\033[92m", 
+            "light_yellow": "\033[93m", "light_blue": "\033[94m"
+        }
         
     def parse_args(self):
-        parser = argparse.ArgumentParser(description='Check apt-get upgrade and format an email')
-        parser.add_argument('--to', action='store', dest="email_to", nargs=1, default=["root",], 
+        parser = argparse.ArgumentParser(description='Check apt-get upgrade and format an email.')
+        subparsers = parser.add_subparsers(dest="sub_command")
+        
+        parser_mail = subparsers.add_parser('mail', help='Outputs the update list packed as email')
+        parser_mail.add_argument('--to', action='store', dest="email_to", nargs=1, default=["root",], 
                             help="Email address to use in the 'to' header", metavar="EMAIL" )
-        parser.add_argument('--from', action='store', dest="email_from", nargs=1, default=["root",],
+        parser_mail.add_argument('--from', action='store', dest="email_from", nargs=1, default=["root",],
                             help="Email address to use in the 'from' header", metavar="EMAIL" )
-        parser.add_argument('--headers', action='store_true', dest="email_headers_enable", default=False, 
+        parser_mail.add_argument('--headers', action='store_true', dest="email_headers_enable", default=False, 
                             help="Enable email header output for direct piping into sendmail" )
+
+        parser_nag = subparsers.add_parser('nagios', help='Act as a nagios plugin.')
+        parser_nag.add_argument('-w', "--warn", action='store', dest="warn", nargs=1, type=int, default=[10,], 
+                            help="Output a warning when there are more than NUM updates available.", metavar="NUM" )
+        parser_nag.add_argument('-c', "--crirical", action='store', dest="critical", nargs=1, type=int, default=[30,], 
+                            help="Ouptut critical status when there are more than NUM updates available.", metavar="NUM" )
+
+        parser_list = subparsers.add_parser('list', help='List the updates in a terminal friendly way')
+        parser_list.add_argument('--no-colors', action='store_true', dest="no_color", default=False, 
+                            help="Do not use colors in terminal output")
+
         args = parser.parse_args()
-        self.email_to = args.email_to[0]
-        self.email_from = args.email_from[0]
-        self.email_enable = args.email_headers_enable
+        if args.sub_command == "mail":
+            self.email_to = args.email_to[0]
+            self.email_from = args.email_from[0]
+            self.email_enable = args.email_headers_enable
+        elif args.sub_command == "nagios":
+            self.warn_thres = args.warn[0]
+            self.critical_thres = args.critical[0]
+        elif args.sub_command == "list":
+            self.use_colors = sys.stdout.isatty() and not args.no_color
+        self.subcommand = args.sub_command
+        
 
     def format_filesize(self, num):
         for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
@@ -43,8 +77,42 @@ class app(object):
         cache.close()
     
     def print_report(self):
+        if self.subcommand == "mail":
+            self.print_email()
+        elif self.subcommand == "nagios":
+            self.print_nagios()
+        elif self.subcommand == "list":
+            self.print_list()
+        
+    def print_nagios(self):
         if self.upgrades == None:
             raise FatalError("updates not loaded")
+        if len(self.upgrades) >= self.warn_thres:
+            self.returncode = 1
+        # critical overrides warn
+        if len(self.upgrades) >= self.critical_thres:
+            self.returncode = 2
+        print("{} Updates available".format(len(self.upgrades)))
+
+    def print_list(self):
+        if self.upgrades == None:
+            raise FatalError("updates not loaded")
+        for pkg in self.upgrades:
+            if self.use_colors:
+                print("{}{}{} ({} => {})".format(
+                    self.colors["green"],
+                    pkg.fullname, 
+                    self.colors["default"],
+                    pkg.installed.version , pkg.candidate.version))
+            else:
+                print("{} ({} => {})".format(pkg.fullname, pkg.installed.version , pkg.candidate.version))
+        
+    def print_email(self):
+        if self.upgrades == None:
+            raise FatalError("updates not loaded")
+        if self.email_enable:
+            self.print_email_headers()
+        
         table = TableWriter.TableWriter()
         table.width = shutil.get_terminal_size( (100,24)  ).columns
         
@@ -87,9 +155,12 @@ class app(object):
         print( "Content-Type: text/plain; charset=\"utf-8\"" )
 
 if __name__ == "__main__":
-    a = app()
-    a.parse_args()
-    a.load_upgrades()
-    if a.email_enable:
-        a.print_email_headers()
-    a.print_report()
+    try:
+        a = app()
+        a.parse_args()
+        a.load_upgrades()
+        a.print_report()
+        exit(a.returncode)
+    except Exception as e:
+        print(e)
+        exit(3)
